621b46cd5c6f8f6117512739a071ecb780a0641e
M Naveen Kumar <naveencoursesonly@gmail.com>
2024-10-15 21:08:37 +0530

    minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './NavBar'; // Import Navbar component
+import Navbar from './NavBar.tsx'; // Import Navbar component
 //import App2 from './App2'; // Import App2 component
-import App2 from './App2';
+import App2 from './App2.tsx';
  // Import Home component or create it if necessary
-import Upload from './Upload';
-import Earnings from './Earnings';
-import PlaylistPage from './PlaylistPage';
+import Upload from './Upload.tsx';
+import Earnings from './Earnings.tsx';
+import PlaylistPage from './PlaylistPage.tsx';
 function App() {
   return (
     <Router>
